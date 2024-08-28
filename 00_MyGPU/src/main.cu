8b9e611f2ea9d97f7e622f16697d176159664e80
xipingya <xiping.yan@intel.com>
2024-08-28 02:38:50 +0000

    Update 00, add list all device info

--- 00_MyGPU/src/main.cu
+++ 00_MyGPU/src/main.cu
@@ -1,5 +1,35 @@
 #include <cuda_runtime_api.h>
+#include <cuda_device_runtime_api.h>
 #include <iostream>
+
+void list_one_gpu_info(int devID) {
+	cudaError_t rt = cudaSetDevice(devID);
+	if (cudaSuccess != rt) {
+		std::cout << "  cudaSetDevice(" << devID << ") fail, return " << rt << std::endl;
+		return;
+	}
+
+	// Get the ID of the currently selected active CUDA device
+	int device = devID;
+	cudaGetDevice(&device);
+
+	// Fetch its properties
+	cudaDeviceProp props;
+	cudaGetDeviceProperties(&props, device);
+
+	/* 
+	We only print the most fundamental properties here. cudaDeviceProp 
+	contains a long range of indicators to check for different things
+	that your GPU may or may not support, as well as factors for 
+	performance. However, the most essential property to know about is
+	the compute capability of the device. 
+	*/
+	std::cout << "  Model: " << props.name << std::endl;
+	std::cout << "  Compute capability: " << props.major << "." << props.minor << std::endl;
+	std::cout << "  Memory: " << props.totalGlobalMem / float(1 << 30) << " GiB" << std::endl;
+	std::cout << "  Multiprocessors: " << props.multiProcessorCount << std::endl;
+	std::cout << "  Clock rate: " << props.clockRate / float(1'000'000) << " GHz" << std::endl;
+}
 
 /*
 Before you use your GPU to do work, you should know the 
@@ -17,27 +47,10 @@ int main()
 		return -1;
 	}
 
-	// Get the ID of the currently selected active CUDA device
-	int device;
-	cudaGetDevice(&device);
-
-	// Fetch its properties
-	cudaDeviceProp props;
-	cudaGetDeviceProperties(&props, device);
-
-	/* 
-	We only print the most fundamental properties here. cudaDeviceProp 
-	contains a long range of indicators to check for different things
-	that your GPU may or may not support, as well as factors for 
-	performance. However, the most essential property to know about is
-	the compute capability of the device. 
-	*/
-	std::cout << "Model: " << props.name << std::endl;
-	std::cout << "Compute capability: " << props.major << "." << props.minor << std::endl;
-	std::cout << "Memory: " << props.totalGlobalMem / float(1 << 30) << " GiB" << std::endl;
-	std::cout << "Multiprocessors: " << props.multiProcessorCount << std::endl;
-	std::cout << "Clock rate: " << props.clockRate / float(1'000'000) << " GHz" << std::endl;
-
+	for (auto n = 0; n < numDevices; n++) {
+		std::cout << "== GPU:" << n << std::endl;
+		list_one_gpu_info(n);
+	}
 	return 0;
 }
 
